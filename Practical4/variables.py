ea14c9c27fd1c2e64340493c18e8e95a896b54a4
myhmzl <yining.23@intl.zju.edu.cn>
2024-03-06 16:16:09 +0800

    v‘ git commit -m v
    git commit -m v‘

---
+++ Practical4/variables.py
@@ -0,0 +1,20 @@
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+if d>e:
+    print("d is greater. Running had a greater improvement on running time.")
+elif d<e:
+    print("e is greater. The combination of running and strength exercises had a greater improvement on running time.")
+else:
+    print("d is as great as e. Two training regime had the same improvement on running time")
+
+# e is greater than d.The combination of running and strength exercises had a greater improvement on running time.
+    
+X=1>0
+Y=1<0
+W=not (X and Y) and (X or Y)
+print(W)
+
+# Z is true.
