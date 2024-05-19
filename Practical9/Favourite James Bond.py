f595eeff83a7b55d3d6ccdec53cd7ffa038c93ff
qiuyining <161458072+qiuyining@users.noreply.github.com>
2024-05-19 22:14:16 +0800

    Practical9

--- Practical9/Favourite James Bond.py
+++ Practical9/Favourite James Bond.py
@@ -1,7 +1,12 @@
+ # Define the function
 def favourite_James_Bond(year_of_birth):
+    # Calculate the year when the person start watching Bond movies
     year_watching_Bond=18+year_of_birth
+    # Define a dictionary including different actors of James Bond in different periods of time 
     bond_actors = {(1973, 1986): "Roger Moore",(1987, 1994): "Timothy Dalton", (1995, 2005): "Pierce Brosnan", (2006, 2021): "Daniel Craig" }
+    # Loop through each time period and associated actor in the dictionary
     for (start_year, end_year), actor in bond_actors.items():
+        # Check if the year the person started watching falls within the current time period
         if start_year <= year_watching_Bond <= end_year:
             return actor
     return "No Bond actor found."
