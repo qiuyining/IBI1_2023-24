038ad8801cb9630a38c217efdaae32f12b338950
qiuyining <161458072+qiuyining@users.noreply.github.com>
2024-05-19 21:52:03 +0800

    Practical9

--- Practical9/Chocolate bar affordability calculator.py
+++ Practical9/Chocolate bar affordability calculator.py
@@ -1,6 +1,10 @@
+# Define the function 
 def chocolate_bar_affordability_calculator(total_money, price):
+    # Calculate the number of bars that can be bought
     number_of_bars = total_money//price
+    # Calculate the remaining money 
     charge = total_money%price
+    # Return the number of bars that can be bought and the leftover money
     return number_of_bars, charge
 # example
 total_money = 100 
