401103f067fa4d9d9d7ddf67388f543fd8ebf188
qiuyining <161458072+qiuyining@users.noreply.github.com>
2024-05-19 21:36:35 +0800

    Practical8

--- Practical8/get_duplicate_genes.py
+++ Practical8/get_duplicate_genes.py
@@ -1,23 +1,35 @@
+# Define the path to the input and output file
 input='D:/IBI/IBI_git/IBI1_2023-24/Practical8/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa'
 output='D:/IBI/IBI_git/IBI1_2023-24/Practical8/duplicate_genes.fa'
 
+# Open the input file for reading
 input_file = open(input, 'r')
+# Open the output file for writing
 output_file = open(output, 'w')
 
 record_sequence = False
 
+# Read the input file line by line
 for line in input_file:
+     # Check if the line starts with '>', indicating a gene name line
     if line.startswith('>'):
+        # Check if 'duplication' is in the line
         if 'duplication' in line:
+            # Extract the gene name
             gene_name = line.split()[0][1:]
+            # Write the gene name to the output file
             output_file.write(f'>{gene_name}\n')
+            # start recording the sequence
             record_sequence = True
+        # If 'duplication' is not in the line, stop recording
         else:
             record_sequence = False
     else:
         if record_sequence:
+            # Write the current line (part of the gene's sequence) to the output file
             output_file.write(line)
 
+# Close the input and output file
 input_file.close()
 output_file.close()
                   
