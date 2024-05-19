1fea765000d5d1cdd33722352346de09761eca06
qiuyining <161458072+qiuyining@users.noreply.github.com>
2024-05-19 21:46:01 +0800

    Practical8

--- Practical8/repetitive_counter.py
+++ Practical8/repetitive_counter.py
@@ -1,7 +1,14 @@
+# Define the sequence
 seq = 'ATGCAATCGGTGTGTCTGTTCTGAGAGGGCCTAA'
+# import necessary library
 import re
+# find all occurrences of GTGTGT or GTCTGT in the sequence
 s=re.finditer(r'(?=(GTGTGT|GTCTGT))',seq)
+# Initialize a counter to zero
 count=0
-for a in s:
+# Go through each found pattern
+for matched_element in s:
+    # Add one to the count for each pattern found
     count+=1
+# Print the total count
 print(count)
