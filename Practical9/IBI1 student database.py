15f662ec5632b6107440b1451a85b713e2d9d14e
qiuyining <161458072+qiuyining@users.noreply.github.com>
2024-05-19 22:18:33 +0800

    Practical9

--- Practical9/IBI1 student database.py
+++ Practical9/IBI1 student database.py
@@ -1,11 +1,14 @@
+# Define a class to show student's information
 class Student:
+     # Initialize a new instance of Student
     def __init__(self, name, major, code_portfolio_score, group_project_score, exam_score):
         self.name = name
         self.major = major
         self.code_portfolio_score = code_portfolio_score
         self.group_project_score = group_project_score
         self.exam_score = exam_score
-    
+        
+    # print the student's information
     def print_details(self):
         print(f"Name: {self.name}, Major: {self.major}, Code Portfolio Score: {self.code_portfolio_score}, Group Project Score: {self.group_project_score}, Exam Score: {self.exam_score}")
 
